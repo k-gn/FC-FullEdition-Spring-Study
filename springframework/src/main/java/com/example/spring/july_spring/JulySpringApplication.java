4bcf67c58a599890c1e219cf1f28ee8dabb78d62
k-gn <rlarbska97@gmail.com>
2021-08-26 14:21:52 +0900

    no message

---
+++ springframework/src/main/java/com/example/spring/july_spring/JulySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.july_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulySpringApplication.class, args);
+    }
+
+}
---
+++ springframework/src/test/java/com/example/spring/july_spring/JulySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.july_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
