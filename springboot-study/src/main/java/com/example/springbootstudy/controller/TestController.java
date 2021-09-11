3ee04e973334a9cecb149fd1cf1f73b8ae6fd29a
k-gn <rlarbska97@gmail.com>
2021-09-11 11:07:21 +0900

    no message

--- springboot-study/src/main/java/com/example/springbootstudy/controller/TestController.java
+++ springboot-study/src/main/java/com/example/springbootstudy/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.example.springbootstudy.controller;
 
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +18,16 @@ public class TestController {
     @PostMapping("/post")
     public void post(String name) {
         System.out.println(name);
+    }
+
+    @GetMapping("/get/str")
+    public ResponseEntity<String> get() throws JSONException {
+        String json =   """
+                    {
+                        "name":"mkyong",
+                        "age":38
+                    }
+                    """;
+        return ResponseEntity.ok(json);
     }
 }
