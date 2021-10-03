03b39538d1857c4d8c9ed99a75a4aec145b7c9c2
k-gn <rlarbska97@gmail.com>
2021-10-03 11:37:24 +0900

    no message

--- boot_getinline/src/main/java/com/example/getinline/controller/error/BaseErrorController.java
+++ boot_getinline/src/main/java/com/example/getinline/controller/error/BaseErrorController.java
@@ -24,7 +24,6 @@ public class BaseErrorController implements ErrorController {
     @RequestMapping(value = "/error", produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView errorHtml(HttpServletResponse response) {
         HttpStatus status = HttpStatus.valueOf(response.getStatus());
-//        HttpStatus status = HttpStatus.BAD_REQUEST;
         ErrorCode errorCode = status.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
         return new ModelAndView("error", Map.of(
                 "statusCode", status.value(),
@@ -37,7 +36,6 @@ public class BaseErrorController implements ErrorController {
     public ResponseEntity<APIErrorResponse> error(HttpServletResponse response) {
         System.out.println("BaseErrorController api");
         HttpStatus status = HttpStatus.valueOf(response.getStatus());
-//        HttpStatus status = HttpStatus.BAD_REQUEST;
         ErrorCode errorCode = status.is4xxClientError() ? ErrorCode.BAD_REQUEST : ErrorCode.INTERNAL_ERROR;
         return ResponseEntity
                 .status(status)
